c86051f0435ddaecf8030296ec33d7e2e5f2113a
Alcabot <144275249+Alcabot24@users.noreply.github.com>
2024-01-27 21:51:42 -0600

    Actualizar gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -4,8 +4,8 @@ const handler = async (m, {conn, args}) => {
   conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
     contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
       title: '𝙻𝙸𝙽𝙺 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾',
-      body: '𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝',
-      previewType: 0, thumbnail: fs.readFileSync('./Menu2.jpg'),
+      body: '𝙰𝙻𝙲𝙰𝙱𝙾𝚃|² ⁴',
+      previewType: 0, thumbnail: fs.readFileSync('./alca.png'),
       sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}});
 };
 handler.help = ['linkgroup'];
